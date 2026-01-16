319fd9b1ed9b17105ca4ec242ed80879bb0a3e09
MolnarKristof0719 <csangcsungcsing@gmail.com>
2026-01-16 08:37:40 +0100

    osszekotesek + barberoffdays tabla

---
+++ server/app/Http/Controllers/BarberOffDayController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\barberOffDay;
+use App\Http\Requests\StorebarberOffDayRequest;
+use App\Http\Requests\UpdatebarberOffDayRequest;
+
+class BarberOffDayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorebarberOffDayRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(barberOffDay $barberOffDay)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatebarberOffDayRequest $request, barberOffDay $barberOffDay)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(barberOffDay $barberOffDay)
+    {
+        //
+    }
+}
