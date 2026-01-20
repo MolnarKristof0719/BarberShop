bdc09bfe12e2468242c1dd0dc304c8e0ea416be3
MolnarKristof0719 <csangcsungcsing@gmail.com>
2026-01-20 08:29:49 +0100

    csv reader

--- server/database/seeders/ServiceSeeder.php
+++ server/database/seeders/ServiceSeeder.php
@@ -2,21 +2,22 @@
 
 namespace Database\Seeders;
 
+use App
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ServiceSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
 
-      $fileName = 'csv/sports.csv';
-        $delimiter = ';';
+    $fileName = 'csv/services.csv';
+    $delimiter = ';';
 
-        $data = CsvReader::csvToArray($fileName, $delimiter);
-        Sports::factory()->createMany($data);
-    }
+    $data = CsvReader::csvToArray($fileName, $delimiter);
+    Sports::factory()->createMany($data);
+  }
 }
