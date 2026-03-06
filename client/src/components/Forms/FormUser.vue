ef4da0a5abe5140a2dcbc521dbe5d4f28072a630
molnarkrsit <csancsungcsing@gmail.com>
2026-03-06 08:15:20 +0100

    roles

--- client/src/components/Forms/FormUser.vue
+++ client/src/components/Forms/FormUser.vue
@@ -83,8 +83,8 @@
             v-model="formItem.role"
           >
             <option value="1">Admin</option>
-            <option value="2">Tanár</option>
-            <option value="3">Diák</option>
+            <option value="2">Barber</option>
+            <option value="3">Customer</option>
           </select>
           <div
             v-if="!serverErrors.role"
